b3f7c8b77e816ef81b1acfb220b321f9c41162c2
AndreRFBaT <andrerfbatista@gmail.com>
2025-03-30 11:19:04 -0300

    NEW: Vue Application

---
+++ test_api/vue-client/src/App.vue
@@ -0,0 +1,11 @@
+<!-- test_api/vue-client/src/App.vue -->
+<script setup>
+import BuscarOperadoras from "./components/BuscarOperadoras.vue";
+</script>
+
+<template>
+  <div>
+    <h1>Consulta de Operadoras</h1>
+    <BuscarOperadoras />
+  </div>
+</template>
